bb9eebc2139027b737b4f9c884fa01544e81e3f2
Zeyana Musthafa <zeyana.a@berkeley.edu>
2017-10-27 19:44:43 -0700

    rem som files

--- Test Cases/I94 - Official Website_files/app.js
+++
@@ -1,8 +0,0 @@
-(function(){
-	
-	'use strict';
-	angular.module('i94', [ 'ngResource', 'ngAnimate', 'ngRoute', 'ngSanitize',
-	                        'ngAria', 'ui.router', 'ui.bootstrap', 'pascalprecht.translate' ]);
-	
-})();
-
